6a43ba3385eea4cf4241990458acad5ac2f7aa40
Natanael de Sousa leite <natanael.leite@serpro.gov.br>
2025-04-22 09:50:36 -0300

    feat: remove dependencias desnecessarias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BrButton } from '@govbr-ds/webcomponents-angular/standalone';
+import { BrButton, BrInput } from '@govbr-ds/webcomponents-angular/standalone';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, BrButton],
+  imports: [RouterOutlet, BrButton, BrInput],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
   title = 'meu-projeto-standalone';
