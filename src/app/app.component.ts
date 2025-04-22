75e14435c3d344dcc5355b7f41610ad19aa93bb2
Natanael de Sousa leite <natanael.leite@serpro.gov.br>
2025-04-22 09:17:58 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrButton } from '@govbr-ds/webcomponents-angular/standalone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BrButton],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'meu-projeto-standalone';
